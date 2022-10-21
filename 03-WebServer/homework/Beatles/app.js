7211741d57059f1819f8808c9f1b510ba758e44b
SantiRosso <santiagorosso@live.com.ar>
2022-10-21 16:49:16 -0300

    finished

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,41 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http.createServer((req, res) => {
+
+  if(req.url === '/') {
+    const index = fs.readFileSync('./index.html', 'utf-8');
+    res.writeHead(200, {'Content-type': 'text/html'}).end(index)
+  }
+
+  const miUrl = req.url.split('/');
+  if(miUrl.length < 3) {
+    const beatleName = miUrl[1].replace('%20', ' ');
+    const beatle = beatles.filter((elem) => elem.name === beatleName)[0];
+    let template = fs.readFileSync('./beatle.html', 'utf-8');
+
+    template = template.replace('{name}', beatle.name);
+    template = template.replace('{birthdate}', beatle.birthdate);
+    template = template.replace('{image}', beatle.profilePic);
+    res.writeHead(200, {'Content-type': 'text/html'}).end(template);
+  }
+
+  if(req.url === '/api') {
+    return res
+      .writeHead(200, {'Content-type': 'application/json'})
+      .end(JSON.stringify(beatles))
+  }
+
+  const beatleName = req.url.split('/').pop().replace('%20', ' ');
+  console.log(beatleName)
+
+  if(req.url.includes('/api') && beatleName) {
+    const beatle = beatles.filter((elem) => elem.name === beatleName)
+    if(!beatle.length) return res.writeHead(404).end('Beatle not found')
+    return res
+      .writeHead(200, {'Content-type': 'application/json'})
+      .end(JSON.stringify(beatle))
+    }
+
+}).listen(3001, 'localhost')
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,17 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res) => {
+    const name = req.url.slice(1);
+    const files = fs.readdirSync('./images');
+
+    for(const file of files) {
+        if(file.includes(`${name}_doge.jpg`)){
+            res.writeHead(200, {'Content-type' : 'image/jpg'});
+            const img = fs.readFileSync(`./images/${name}_doge.jpg`);
+            return res.end(img);
+        }
+    }
+    return res.writeHead(404).end('Not found');
+})
+.listen(3001, 'localhost')
